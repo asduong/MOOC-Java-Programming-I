aec06e276db24d1d1e6db163817f4b76121725bd
Andrew Duong <duongandrews@gmail.com>
2020-03-30 15:13:25 -0400

    Week 1

---
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class GiftTax {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int gift = Integer.valueOf(scan.nextLine());
+        if (gift >= 5000 && gift < 25000) {
+            System.out.println(100 + (gift - 5000) * 0.08);
+        } else if (gift >= 25000 && gift < 55000) {
+            System.out.println(1700 + (gift - 25000) * 0.1);
+        } else if (gift >= 55000 && gift < 200000) {
+            System.out.println(4700 + (gift - 55000) * 0.12);
+        }else if (gift >= 200000 && gift < 1000000) {
+            System.out.println(22100 + (gift - 200000) * 0.15);
+        } else if (gift >= 1000000) {
+            System.out.println(142100 + (gift - 1000000) * 0.17);
+        } else {
+            System.out.println("No tax!");
+        }
+    }
+}
